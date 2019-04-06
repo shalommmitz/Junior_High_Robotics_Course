08771de216b0626c9b5b97a63b3e33ed92f64d54
jon <shalommmitz@gmail.com>
2019-04-06 21:46:09 +0300

    Fixed convert_bmp_to_hex_Image to handle color images

--- robot_PC_software/robot_pics/convert_bmp_to_hex_Image.py
+++ robot_PC_software/robot_pics/convert_bmp_to_hex_Image.py
@@ -23,16 +23,23 @@ im = Image.open(fn)
 x = 0
 y = 0
 width, height = im.size
+if width != 64:
+    print("ERROR: width must be equal to 64 - Aborting.")
+    exit()
+if height != 48:
+    print("ERROR: height must be exactly 48 - Aborting.")
+    exit()
+
 f = open(outFN, 'w')
 for y in range(0,height):
     line = ""
-    for x_byte in range(0,width, 4):
+    for x_nibble in range(0,width, 4):
         c = 0
         for x in range(4):
-            pixel = im.getpixel((x_byte+x,y))
-            #print x_byte, x, y
+            pixel = im.getpixel((x_nibble+x,y))
+            #print x_nibble, x, y, pixel
             c = c*2
-            if pixel==0:
+            if pixel==0 or pixel==(0,0,0):
                 c += 1
                 line += "X"
             else:
